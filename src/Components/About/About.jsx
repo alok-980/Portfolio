547830559f5a1e91cda8fa4f7ab32088410682e6
Alok Chauhan <alokchuhan7896@gmail.com>
2025-03-12 07:55:02 +0530

    changin the profile picture

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './About.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
-import profile_img from '../../assets/about_profile.svg';
+import profile_img from '../../assets/profile picture(1).jpg';
 
 function About() {
     return (
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Hero.css';
-import profile_img from '../../assets/profile_img.svg';
+import profile_img from '../../assets/profile picture.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function Hero() {
